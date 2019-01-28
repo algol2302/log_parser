603a7d5904ad7a1506d9bca3f88fb61af1fea856
algol2302 <algol.mail@gmail.com>
2019-01-29 01:03:02 +0700

    feat (core): add statistics in api

--- core/api/viewsets/parsed_data.py
+++ core/api/viewsets/parsed_data.py
@@ -2,11 +2,6 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import filters
 from django_filters import rest_framework as django_filters
-# from django.contrib.auth.models import User
-# from django.views.decorators.cache import cache_page
-# from django.utils.decorators import method_decorator
-# from django.conf import settings
-# from django.core.cache import caches
 
 from core.models import ParsedData
 from core.api.serializers import ParsedDataSerializer
